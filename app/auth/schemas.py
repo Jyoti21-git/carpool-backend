d9e6ccfc953f07420a918cbea875db8da2b13220
Jyoti Pundir <jyoti.pundir2109@gmail.com>
2026-06-22 12:32:31 +0530

    Fix VerifyOtpRequest

--- app/auth/schemas.py
+++ app/auth/schemas.py
@@ -6,6 +6,11 @@ class SendOtpRequest(BaseModel):
     email: EmailStr
 
 
+class VerifyOtpRequest(BaseModel):
+    email: EmailStr
+    otp: str
+
+
 class SetPasswordRequest(BaseModel):
     email: EmailStr
     password: str
